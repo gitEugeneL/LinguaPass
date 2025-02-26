ccf613f2cc99fc6591baa0c2d38490bbca9256b3
Eugene L. <euglihon.dev@gmail.com>
2025-02-26 19:11:29 +0100

    added generateCode integration tests

--- Services/Identity/IdentityApi/Features/GenerateCode/Handler.cs
+++ Services/Identity/IdentityApi/Features/GenerateCode/Handler.cs
@@ -9,13 +9,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityApi.Features.GenerateCode;
 
-internal class Handler(
+public class Handler(
     AppDbContext dbContext,
     IValidator<Command> validator,
     ISecurityService securityService,
     IConfirmationService confirmationService
 ) : IRequestHandler<Command, Result<Output>>
 {
+    public const string InvalidData = "User is not found or account is locked";
+
     public async Task<Result<Output>> Handle(Command command, CancellationToken ct)
     {
         var validationResult = await validator.ValidateAsync(command, ct);
@@ -28,14 +30,14 @@ internal class Handler(
             .FirstOrDefaultAsync(u => u.Email == command.Email.ToUpper(), ct);
 
         if (user is null || confirmationService.IsConfirmLocked(user))
-            return Result<Output>.Failure(new Error("User is not found or account is locked"));
+            return Result<Output>.Failure(new Error(InvalidData));
 
         user.GenerateCodeCount++;
 
         if (confirmationService.IsGenerateCodeAttemptLimitExceeded(user))
         {
             await dbContext.SaveChangesAsync(ct);
-            return Result<Output>.Failure(new Error("Too many code generation attempts"));
+            return Result<Output>.Failure(new Error(InvalidData));
         }
 
         var confirmationCode = user.ConfirmationCode;
---
+++ Services/Identity/Tests/IdentityApi.IntegrationTests/Features/GenerateCodeTests.cs
@@ -0,0 +1,84 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using IdentityApi.Contracts;
+using IdentityApi.Features.GenerateCode;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityApi.IntegrationTests.Features;
+
+public class GenerateCodeTests(CustomWebAppApplicationFactory factory) : IClassFixture<CustomWebAppApplicationFactory>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+    private readonly IConfiguration _configuration = factory.Services.GetRequiredService<IConfiguration>();
+
+    [Theory]
+    [InlineData("mailt@mail.test", "strongPwd!1")]
+    [InlineData("mail1@mail.test", "myPassword12@")]
+    public async Task GenerateCode_WithValidUser_CrateCodeAndReturnsInfo(string email, string password)
+    {
+        // Arrange
+        var codeLifeTimeMinutes = int.Parse(_configuration["Authentication:Code.Lifetime.Minutes"]!);
+
+        await TestExtensions.RegistrationAsync(_client, email, password, password);
+        var request = new GenerateCoreRequest(email);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("generate-code", request);
+        var result = await TestExtensions.DeserializeResponse<GenerateCodeResponse>(response);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Email.Should().Be(email);
+        result.CodeExpires.Should()
+            .BeCloseTo(DateTime.UtcNow.AddMinutes(codeLifeTimeMinutes), TimeSpan.FromSeconds(3));
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Theory]
+    [InlineData("mailt123@mail.test")]
+    [InlineData("mail1321@mail.test")]
+    public async Task GenerateCode_WithInvalidUser_ReturnsErrorMessage(string email)
+    {
+        // Arrange
+        var request = new GenerateCoreRequest(email);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("generate-code", request);
+        var result = await TestExtensions.DeserializeResponse<string>(response);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().Be(Handler.InvalidData);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Theory]
+    [InlineData("mailt@dev.test", "strongPwd!1")]
+    [InlineData("mail1@dev.test", "myPassword12@")]
+    public async Task GenerateCode_WithValidUserAndMultipleAttempts_ReturnsErrorMessage(string email, string password)
+    {
+        // Arrange
+        var codeMaxAttempts = int.Parse(_configuration["Authentication:Code.MaxAttempts"]!);
+
+
+        await TestExtensions.RegistrationAsync(_client, email, password, password);
+        var request = new GenerateCoreRequest(email);
+
+        // Act
+        var response = new HttpResponseMessage();
+        for (var i = 0; i <= codeMaxAttempts; i++)
+            response = await _client.PostAsJsonAsync("generate-code", request);
+
+        var result = await TestExtensions.DeserializeResponse<string>(response);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().Be(Handler.InvalidData);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+}
