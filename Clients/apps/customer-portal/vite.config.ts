54e6f3b4480b8e45f28c0e9a586c46d8938fa840
Eugene L. <euglihon.dev@gmail.com>
2025-08-18 20:29:55 +0200

    feat(customer-portal): add service worker setup with Workbox for caching and offline support

--- Clients/apps/customer-portal/vite.config.ts
+++ Clients/apps/customer-portal/vite.config.ts
@@ -2,9 +2,47 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { type ManifestOptions, VitePWA } from 'vite-plugin-pwa';
+
+const manifest: Partial<ManifestOptions> | false = {
+  theme_color: '#2B2A3AFF',
+  background_color: '#2B2A3AFF',
+  icons: [
+    {
+      purpose: 'maskable',
+      sizes: '512x512',
+      src: 'icons/icon512_maskable.png',
+      type: 'image/png'
+    },
+    {
+      purpose: 'any',
+      sizes: '512x512',
+      src: 'icons/icon512_rounded.png',
+      type: 'image/png'
+    }
+  ],
+  orientation: 'any',
+  display: 'standalone',
+  lang: 'en-US',
+  name: 'customer-portal',
+  short_name: 'Customer',
+  start_url: '/'
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{html,css,js,ico,png,svg}']
+      },
+      manifest: manifest
+    })
+  ],
   server: {
     port: 5173,
     host: true
